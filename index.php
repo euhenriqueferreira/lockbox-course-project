80e1980e91907d12a0e360fa8dc3f8fe93cab455
Henrique <henriqueferreira0320@gmail.com>
2024-11-06 17:25:24 -0300

    Caixa de Pesquisa

--- index.php
+++ index.php
@@ -6,7 +6,31 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Book Wise</title>
 </head>
-<body>
-    hello world
+<body class="bg-stone-950 text-stone-200">
+    <header class="bg-stone-900">
+        <nav class="flex justify-between py-4 max-w-screen-lg mx-auto">
+            <div class="font-bold text-xl tracking-wide">Book Wise</div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="/" class="text-lime-500">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+            <ul>
+                <li><a href="/login.php" class="hover:underline">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="mx-auto max-w-screen-lg space-y-6">
+        <h1 class="mt-6 font-bold text-lg">Explorar</h1>
+        <form class="w-full flex space-x-2 mt-6">
+            <input type="text" class="border-stone-800 border-2 rounded-md bg-stone-900 text-sm focus:outline-none px-2 py-1" placeholder="Pesquisar..." name="pesquisar">
+            <button type="submit">🔎</button>
+        </form>
+
+
+        <div>
+            lista final
+        </div>
+    </main>
 </body>
 </html>
