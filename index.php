d4ccb7d388e3e0fd22da5afdac4f5a44c8f20484
Henrique <henrique@wopus.com.br>
2024-11-06 23:34:23 -0300

    Functions, Controllers e Rotas

--- index.php
+++ index.php
@@ -1,19 +1,7 @@
 <?php
+    // Importando arquivo de funções
+    require 'functions.php';
+
     // Request da página sempre vai cair no index.php (a página não troca)
-
-    // O Controller padrão é index, pois ele controla a página inicial
-    $controller = 'index';
-    
-
-    // echo '<pre>';
-    //     var_dump($_SERVER);
-    // echo '<pre>';
-    
-    // Se na URL tiver o link de algum outro arquivo/página, ele carrega o respectivo controller dessa página
-    if(isset($_SERVER['PATH_INFO'])){
-        $controller = str_replace('/','', $_SERVER['PATH_INFO']);
-    }
-
-    // Carregando o controller dinamicamente
-    require "controllers/{$controller}.controller.php";
+    require 'routes.php';
 ?>
